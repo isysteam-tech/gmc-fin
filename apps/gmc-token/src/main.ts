d2db4102f15b747833173d2b93acb022904e913d
suseendhiran7 <suseendhiran7@gmail.com>
2025-10-16 07:10:36 +0530

    add dotenv

--- apps/gmc-token/src/main.ts
+++ apps/gmc-token/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Server running on:${process.env.PORT || 3000}`);
+  console.log(`Server running on:${process.env.PORT || 3001}`);
 }
 bootstrap();
